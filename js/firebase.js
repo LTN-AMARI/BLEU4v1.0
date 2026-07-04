6001f58fb0678d7b07e3fe55053425cc0db12b61
LTN-AMARI <jc.batimoz@gmail.com>
2026-07-04 01:32:04 -1000

    Refactor Firebase initialization and exports
    Removed unused Firebase authentication and configuration details. Updated exports for database functions.

--- js/firebase.js
+++ js/firebase.js
@@ -1,58 +1,21 @@
-// =======================
-// FIREBASE INIT BLEU4 V2
-// =======================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
 import {
     getDatabase,
     ref,
-    push,
     set,
     onValue,
     update,
     remove
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-import {
-    getAuth,
-    signInAnonymously
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-// =======================
-// CONFIG
-// =======================
 const firebaseConfig = {
-    apiKey: "AIzaSyCOTja98aA-0umXrqm2c2k4frUFn6why1o",
-    authDomain: "bleu-4.firebaseapp.com",
-    databaseURL: "https://bleu-4-default-rtdb.firebaseio.com",
-    projectId: "bleu-4",
-    storageBucket: "bleu-4.appspot.com",
-    messagingSenderId: "788139266954",
-    appId: "1:788139266954:web:c1896f25eb57687846ae73"
+    apiKey: "XXX",
+    authDomain: "XXX",
+    databaseURL: "XXX",
+    projectId: "XXX",
 };
 
-// =======================
-// INIT
-// =======================
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app);
+export const db = getDatabase(app);
 
-// connexion anonyme obligatoire
-signInAnonymously(auth)
-    .then(() => console.log("Firebase OK"))
-    .catch(err => console.error("Auth error", err));
-
-// =======================
-// EXPORTS
-// =======================
-export {
-    db,
-    ref,
-    push,
-    set,
-    onValue,
-    update,
-    remove
-};
+export { ref, set, onValue, update, remove };
