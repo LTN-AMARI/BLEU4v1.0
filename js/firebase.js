20b8ab9790863ae04fefb8dc1e21a1ce85c9a3d3
LTN-AMARI <jc.batimoz@gmail.com>
2026-07-04 01:47:12 -1000

    Update Firebase configuration with new project details

--- js/firebase.js
+++ js/firebase.js
@@ -9,10 +9,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "XXX",
-    authDomain: "XXX",
-    databaseURL: "XXX",
-    projectId: "XXX",
+  apiKey: "AIzaSyCOTja98aA-0umXrqm2c2k4frUFn6why1o",
+  authDomain: "bleu-4.firebaseapp.com",
+  databaseURL: "https://bleu-4-default-rtdb.firebaseio.com",
+  projectId: "bleu-4",
+  storageBucket: "bleu-4.firebasestorage.app",
+  messagingSenderId: "788139266954",
+  appId: "1:788139266954:web:c1896f25eb57687846ae73"
 };
 
 const app = initializeApp(firebaseConfig);
